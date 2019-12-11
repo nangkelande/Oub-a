108d7ce5631af7a4a664128598713a8fb12b95eb
kratos-sauce <57620361+kratos-sauce@users.noreply.github.com>
2019-12-11 18:09:39 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,19 +123,13 @@ async def pipcheck(pip):
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
     await on.edit("`"
-                     "i am ON My Mastor \n\n"
-                     " \n\n"
-                     "botdo is ON \n\n"
+                     "i am ON broh \n\n"
+                     "and mybot is ON \n\n"
                      f"Telethon version: {version.__version__} \n"
                      f"Python: {python_version()} \n"
-                     f"------------------------------------ \n"
+                     f"-------------------- \n"
                      f"User: {DEFAULTUSER} \n"
-                     " \n\n"
-                     f"Creator: Mayur Karaniya \n"
-                     " \n\n"
-                     f"Owner: 3Cube TeKnoways \n"
-                     " \n\n"
-                     f"Userbot: testuserbot "
+                     f"jangan lupa coly "
                      "`")    
 
 
